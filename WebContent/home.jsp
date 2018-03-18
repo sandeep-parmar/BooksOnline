c018f4a962ec2babb7ed6999b5c68af4bc650757
ashutosh <ashurulz7@gmail.com>
2018-03-18 11:50:24 +0530

    import added as giving errors

--- WebContent/home.jsp
+++ WebContent/home.jsp
@@ -1,13 +1,14 @@
-<%@page import="com.dao.*"%>
 <%@page import="com.dao.DBFacade"%>
-<%@page import="java.util.ArrayList"%>
+<%@page import="com.bean.BookUser"%>
+<%@page import="com.dao.*"%>
 <%@page import="com.bean.*"%>
 <%@page import="java.util.List"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %> 
+<%@page import="java.util.ArrayList"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<%@ taglib prefix="shiro" uri="http://shiro.apache.org/tags" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="shiro" uri="http://shiro.apache.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -26,61 +27,99 @@
 </head>
 <body>
 
-<%@ include file="navbar.jsp"%>
+	<%@ include file="navbar.jsp"%>
 
-<% 
-	List<BookUser> list = DBFacade.getBookAdList();
-	request.setAttribute("list", list);
-%>
-<div class="container-fluid bookcontainer">
- 
-  <div class="row">
-  
-  <c:forEach items="${list}" var = "item">
-  	<div class="col-sm-3"> 
-  		<div class="panel panel-primary">    
-    		<div class="panel-body">
-    			<img 
-    				class = "img-responsive imgstyle center-block" 
-    				src = <c:out value = "${item.getBook().getThumbnail()}"/> 
-    				alt = <c:out value = "${item.getBook().getThumbnail()}"/>
-    			>
-    		</div>
-    		<div class="panel-footer">
-				<div class="row">
-					<div class="col-sm-4"><h5><b>Author</b></h5></div>
-					<div class="col-sm-8"><h5><c:out value = "${item.getBook().getAuthors()}"/></h5></div>
+	<%
+		List<BookUser> list = DBFacade.getBookAdList();
+		request.setAttribute("list", list);
+	%>
+	<div class="container-fluid bookcontainer">
+
+		<div class="row">
+
+			<c:forEach items="${list}" var="item">
+				<div class="col-sm-3">
+					<div class="panel panel-primary">
+						<div class="panel-body">
+							<img class="img-responsive imgstyle center-block"
+								src=<c:out value = "${item.getBook().getThumbnail()}"/>
+								alt=<c:out value = "${item.getBook().getThumbnail()}"/>>
+						</div>
+						<div class="panel-footer">
+							<div class="row">
+								<div class="col-sm-4">
+									<h5>
+										<b>Author</b>
+									</h5>
+								</div>
+								<div class="col-sm-8">
+									<h5>
+										<c:out value="${item.getBook().getAuthors()}" />
+									</h5>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-4">
+									<h5>
+										<b>Title</b>
+									</h5>
+								</div>
+								<div class="col-sm-8">
+									<h5>
+										<c:out value="${item.getBook().getTitle()}" />
+									</h5>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-4">
+									<h5>
+										<b>Offer Price</b>
+									</h5>
+								</div>
+								<div class="col-sm-8">
+									<h5>
+										<c:out value="${item.getPrice()}" />
+									</h5>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-4">
+									<h5>
+										<b>City</b>
+									</h5>
+								</div>
+								<div class="col-sm-8">
+									<h5>
+										<c:out value="${item.getLocality().getCity()}" />
+									</h5>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-4">
+									<h5>
+										<b>Locality</b>
+									</h5>
+								</div>
+								<div class="col-sm-8">
+									<h5>
+										<c:out value="${item.getLocality().getArea()}" />
+									</h5>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class="row">
-					<div class="col-sm-4"><h5><b>Title</b></h5></div>
-					<div class="col-sm-8"><h5><c:out value = "${item.getBook().getTitle()}"/></h5></div>
-				</div>
-				<div class="row">
-					<div class="col-sm-4"><h5><b>Offer Price</b></h5></div>
-					<div class="col-sm-8"><h5><c:out value = "${item.getPrice()}"/></h5></div>
-				</div>
-				<div class="row">
-					<div class="col-sm-4"><h5><b>City</b></h5></div>
-					<div class="col-sm-8"><h5><c:out value = "${item.getLocality().getCity()}"/></h5></div>
-				</div>
-				<div class="row">
-					<div class="col-sm-4"><h5><b>Locality</b></h5></div>
-					<div class="col-sm-8"><h5><c:out value = "${item.getLocality().getArea()}"/></h5></div>
-				</div>								
-    		</div>
-  		</div>
-  	</div>
-  	</c:forEach> 	
+			</c:forEach>
+		</div>
+
 	</div>
-	
-</div>
 
 
 
-<!-- Footer -->
-<footer class="container-fluid bg-4 text-center">
-  <p>Online second hand bookstore </p> 
-</footer>
+	<!-- Footer -->
+	<footer class="container-fluid bg-4 text-center">
+	<p>Online second hand bookstore</p>
+	</footer>
 
 </body>
 </html>
--- target/classes/database/ddl_01.sql
+++ target/classes/database/ddl_01.sql
@@ -1,31 +1,140 @@
-create database bookDB;
-\
-CREATE TABLE `bookdb`.`books` (
-  `bookid` VARCHAR(100) NOT NULL,
-  `booktitle` VARCHAR(500) NOT NULL,
-  `bookauthor` VARCHAR(500) NOT NULL,
-  `bookdesc` VARCHAR(2000) NOT NULL,
-  `thumbnail` VARCHAR(500) NOT NULL,
-  PRIMARY KEY(`bookid`)
-);
-\
-CREATE TABLE `bookdb`.`user` (
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `mobile` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `salt` VARCHAR(45) NOT NULL,
-  `uuid` VARCHAR(45) NOT NULL,
-  `active` INTEGER UNSIGNED NOT NULL
-)
-\
---added 17feb
-CREATE TABLE `bookdb`.`bookad` (
-  `isbn` VARCHAR(45) NOT NULL,
-  `usermobile` VARCHAR(45) NOT NULL,
-  `useremail` VARCHAR(45) NOT NULL,
-  `offerprice` VARCHAR(45) NOT NULL,
-  `preferredloc` VARCHAR(45) NOT NULL
-)
-ENGINE = InnoDB;
-ALTER TABLE `bookdb`.`bookad` ADD PRIMARY KEY(`isbn`, `usermobile`, `useremail`, `offerprice`, `preferredloc`);
+-- MySQL dump 10.13  Distrib 5.5.50, for Win32 (x86)
+--
+-- Host: localhost    Database: bookdb
+-- ------------------------------------------------------
+-- Server version	5.5.50
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book_user`
+--
+
+DROP TABLE IF EXISTS `book_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_user` (
+  `uid` varchar(64) NOT NULL DEFAULT '',
+  `bookid` varchar(64) NOT NULL DEFAULT '',
+  `pin` int(6) DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `phone` varchar(12) DEFAULT NULL,
+  `price` int(8) DEFAULT NULL,
+  `soldstatus` int(1) DEFAULT NULL,
+  PRIMARY KEY (`uid`,`bookid`),
+  KEY `pin` (`pin`),
+  CONSTRAINT `book_user_ibfk_1` FOREIGN KEY (`pin`) REFERENCES `locality` (`pin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_user`
+--
+
+LOCK TABLES `book_user` WRITE;
+/*!40000 ALTER TABLE `book_user` DISABLE KEYS */;
+INSERT INTO `book_user` VALUES ('123','9780062515674',411015,'sandeep parmar','9011916677',150,0),('123','9789352770250',411015,'sandeep parmar','9011916677',200,1),('123','9789386057587',452145,'sandeep parmar','9011916677',75,0);
+/*!40000 ALTER TABLE `book_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `bookid` varchar(13) NOT NULL DEFAULT '',
+  `booktitle` varchar(64) NOT NULL,
+  `bookauthor` varchar(1024) NOT NULL,
+  `bookdesc` varchar(2048) DEFAULT NULL,
+  `thumbnail` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`bookid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES ('9780062515674','The Monk Who Sold His Ferrari','Robin Sharma ','Wisdom to Create a Life of Passion, Purpose, and Peace This inspiring tale provides a step-by-step approach to living with greater courage, balance, abundance, and joy. A wonderfully crafted fable, The Monk Who Sold His Ferrari tells the extraordinary story of Julian Mantle, a lawyer forced to confront the spiritual crisis of his out-of-balance life. On a life-changing odyssey to an ancient culture, he discovers powerful, wise, and practical lessons that teach us to: Develop Joyful Thoughts, Follow Our Life\'s Mission and Calling, Cultivate Self-Discipline and Act Courageously, Value Time as Our Most Important Commodity, Nourish Our Relationships, and Live Fully, One Day at a Time.','http://books.google.com/books/content?id=aKJAQGed_-MC&printsec=frontcover&img=1&zoom=1&source=gbs_api'),('9789352770250','And Then There Were None','Agatha Christie ','Ten strangers, apparently with little in common, are lured to an island mansion by the mysterious U.N.Owen. Over dinner, a record begins to play, accusing each of hiding a guilty secret. Then former reckless driver Tony Marston is found murdered by a deadly dose of cyanide. The tension escalates as the survivors realize that the killer is not only among them but is preparing to strike again... and again...','http://books.google.com/books/content?id=BOw_DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'),('9789386057587','Inner Engineering','Sadhguru ','‘Inner Engineering is a fascinating read, rich with Sadhguru’s insights and his teachings. If you are ready, it is a tool to help awaken your own inner intelligence, the ultimate and supreme genius that mirrors the wisdom of the cosmos’—Deepak Chopra In his revolutionary new book, visionary, mystic and yogi Sadhguru distils his own experiences with spirituality and yoga and introduces the transformational concept of Inner Engineering. Developed by him over several years, this powerful practice serves to align the mind and the body with energies around and within, creating a world of limitless power and possibilities. Inner Engineering is your own software for joy and well-being.','http://books.google.com/books/content?id=cqCiDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locality`
+--
+
+DROP TABLE IF EXISTS `locality`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locality` (
+  `pin` int(6) NOT NULL,
+  `city` varchar(64) DEFAULT NULL,
+  `area` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`pin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locality`
+--
+
+LOCK TABLES `locality` WRITE;
+/*!40000 ALTER TABLE `locality` DISABLE KEYS */;
+INSERT INTO `locality` VALUES (411015,'PUNE','vishrantwadi'),(452145,'chennai','manda'),(654124,'chennai','manda');
+/*!40000 ALTER TABLE `locality` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `mobile` varchar(13) NOT NULL DEFAULT '',
+  `email` varchar(64) NOT NULL,
+  `salt` varchar(256) DEFAULT NULL,
+  `uuid` varchar(256) DEFAULT NULL,
+  `active` int(11) DEFAULT NULL,
+  PRIMARY KEY (`mobile`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('sandeep','cwvnwe0d2vWzru5m6re64lEje53h9lZq56DCjoOQQI4=','123','sandeepparmar20@gmail.com','wLM8EHTQrjeaXfkWbHu8Ug==','4c1bd10f-540a-4436-a2b6-bbfa2e19ed4c',1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-26 20:15:21
